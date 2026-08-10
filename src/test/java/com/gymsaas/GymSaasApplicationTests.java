96a30f99a054972331cd1005e56e1d99cd5b82e1
Bruno <brunix.palombarini@gmail.com>
2026-08-10 18:30:30 -0300

    fix ci/cd y test

--- src/test/java/com/gymsaas/GymSaasApplicationTests.java
+++ src/test/java/com/gymsaas/GymSaasApplicationTests.java
@@ -2,12 +2,15 @@ package com.gymsaas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GymSaasApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Verifica que el contexto de Spring levanta correctamente
+		// usando H2 en memoria (profile test)
 	}
-
 }
